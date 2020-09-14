42ac8f60530998cb46dd3865a092ace47b31074a
komsorn thongboonma <komsorn.t@ku.th>
2020-09-14 21:44:30 +0700

    initial commit

---
+++ src/main/java/atm/Main.java
@@ -0,0 +1,12 @@
+package atm;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean.xml");
+        ATM  atm = context.getBean(ATM.class);
+        Bank bank = context.getBean(Bank.class);
+    }
+}
